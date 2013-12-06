31aa12781ca27ce6906f127b9c2e2ae59c582795
n0ts <me@n0ts.org>
2013-12-06 19:03:53 +0900

    Added metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'sensu_community_plugins'
+maintainer       'Ulf Mansson'
+maintainer_email 'ulf.mansson@recordedfuture.com'
+license          'Apache 2.0'
+description      'Installs the community plugins for Sensu'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+supports         'ubuntu', '>= 0.0.0'
+depends          'git', '>= 0.0.0'
+depends          'sensu'
